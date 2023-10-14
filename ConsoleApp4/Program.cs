804e729df19b25b1287c227459722ffd36ce3758
cerf01 <145259390+cerf01@users.noreply.github.com>
2023-10-14 15:53:08 +0200

    Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ConsoleApp4
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp4
 {
     class Program
     {
@@ -9,10 +12,11 @@
             int min = 0;
             int max = 10;
             int count = 1;
-            while (q != 'q')
+            q = ' ';
+            while (q != 'e')
             {
-                Console.Clear();
-                Console.WriteLine("Choose task: [1 - 5]");
+               Console.Clear();
+                Console.WriteLine("Choose task: [1 - 5] (e - exit)");
                 q = Console.ReadKey().KeyChar;
                 Console.WriteLine();
                 switch (q)
@@ -20,16 +24,14 @@
                     case '1':
                         {
                             task1();
-                        }
-                        break;
-
+                        }break;
                     case '2':
                         {
                             Console.WriteLine("Enter min and max numbers");
                             min = Convert.ToInt32(Console.ReadLine());
                             max = Convert.ToInt32(Console.ReadLine());
                             if (min > max)
-                            {                            
+                            {
                                 while (min > max)
                                 {
                                     Console.WriteLine("wrong values!");
@@ -42,34 +44,58 @@
                             task2(min, max);
                         }
                         break;
-
                     case '3':
                         {
+
+                            Console.WriteLine("Enter min and max numbers:");
+                            min = Convert.ToInt32(Console.ReadLine());
+                            max = Convert.ToInt32(Console.ReadLine());
+                            if (min > max)
+                                while (min > max)
+                                {
+                                    Console.WriteLine("wrong values! pls try again");
+                                    Console.WriteLine("Enter min and max numbers:");
+                                    min = Convert.ToInt32(Console.ReadLine());
+                                    max = Convert.ToInt32(Console.ReadLine());
+                                }
+
                             count = Convert.ToInt32(Console.ReadLine());
-                            task3(count, min ,max);
+                            Console.WriteLine("Enter count of threads:");
+                            if (count == 0)
+                                while (count == 0)
+                                {
+                                    Console.WriteLine("counter is zero! pls try again");
+                                    count = Convert.ToInt32(Console.ReadLine());
+
+                                }
+                            task3(count, min, max);
                         }
                         break;
-
                     case '4':
                         {
-
+                            
+                            task4();
                         }
                         break;
-
                     case '5':
                         {
 
                         }
                         break;
-
-                    case 'q':
+                    case 'e':
                         {
-                            q = 'q';
-                            break;
+                          
+                            q = 'e';
+                        }
+                        break;
+                  
+                    default:
+                        {
+                            Console.WriteLine("Error input!");
+                            Console.ReadKey();
                         }
                         break;
                 }
-
             }
         }
         private static void forTask2(object obj) 
@@ -81,6 +107,7 @@
             var arr = new int[size];
             for (int i = 0; i < size; i++)
                 arr[i] = min+i;
+
             foreach (int i in arr)
             {
                 Console.WriteLine($"Thread: \t{i}");
@@ -94,6 +121,7 @@
                 var arr = new int[51];
                 for (int i = 0; i < 51; i++)
                     arr[i] = i;
+
                 foreach (int i in arr)
                 {
                     Console.WriteLine($"Thread: \t{i}");
@@ -104,17 +132,71 @@
         }
         private static void task2(int min, int max)
         {
-            var arr = new int[2];
-        
+            var arr = new int[2];       
                 arr[0] = min;
                  arr[1]= max;
+
             Thread thread = new Thread(new ParameterizedThreadStart(forTask2));
             thread.Start(arr);
+
             Console.ReadKey();
         }
         private static void task3(int count, int min, int max) 
         {
+            int mult = 0;
+            int size = (max - min) +1;
+            var arr = new int[size];
 
+            for (int i = 0; i < size; i++)
+                arr[i] = min + i;
+
+            for (int i = 0; i < count; i++)
+            {
+                Thread thread = new Thread(() =>
+                {
+                    for (int j = mult; j <  size/count+mult; j++)
+                            Console.WriteLine($"Thread {i + 1}: \t{arr[j]}\t{mult}");                  
+                });
+                thread.Start();              
+                thread.Join();
+                mult += size/count;
+            }
+
+            Console.ReadKey();
+        }
+        private static void task4() 
+        {
+            var list =new List<int>();
+            for(int i =0;i<1000;i++)
+                list.Add(new Random().Next(-10,100));
+            Thread minimalValue = new Thread(() =>
+            {
+                int min = list[0];
+                foreach (var value in list)
+                    if (value < min)
+                        min = value;
+                Console.WriteLine($"Minimal value is: {min}");       
+            });
+            Thread maximalValue = new Thread(() =>
+            {
+                int max = list[0];
+                foreach (var value in list)
+                    if (value > max)
+                        max = value;
+                Console.WriteLine($"Maximal value is: {max}");
+            });
+            Thread averageValue = new Thread(() =>
+            {               
+                int sum = 0;
+                foreach (var value in list)
+                  sum+= value;
+                double avg = sum/list.Count;
+                Console.WriteLine($"Average is: {avg}");
+            });
+            minimalValue.Start();
+            maximalValue.Start();
+            averageValue.Start();
+            Console.ReadKey();
         }
     }
 }
